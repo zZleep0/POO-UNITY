e123afb8210ba0b084313d102828ea30d9ec74f0
zZleep0 <152449401+zZleep0@users.noreply.github.com>
2024-11-14 21:33:03 -0300

    Rotacao de camera

--- POO-IN-UNITY/Assets/Scripts/Photonn/ShowPlayerNickname.cs
+++ POO-IN-UNITY/Assets/Scripts/Photonn/ShowPlayerNickname.cs
@@ -6,17 +6,32 @@ using UnityEngine.UI;
 
 public class ShowPlayerNickname : MonoBehaviourPunCallbacks
 {
+    private Camera playerCamera;
+
     // Start is called before the first frame update
     void Start()
     {
         //Definir o nome do jogador
         GetComponent<Text>().text = photonView.Owner.NickName.Split(" | ")[0];
         Debug.Log(photonView.Owner.NickName.Split(" | ")[0]);
+
+        //Procurando a camera do jogador
+        playerCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerCamera != null && !photonView.IsMine)
+        {
+            //Faz o nome olhar para a camera, com rotacao adequada
+            transform.LookAt(playerCamera.transform.position);
+
+            //Mantem a rotacao correta, apenas para nao inverter o nome
+            Vector3 eulerRotation = transform.rotation.eulerAngles;
+            eulerRotation.x = 0; //Previne a rotacao em x
+            eulerRotation.y = 0; //Previne a rotacao em y
+            transform.rotation = Quaternion.Euler(eulerRotation);
+        }
     }
 }
